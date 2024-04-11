fef5ecb2da5ebc62bb358b87b7e40691e506c3f9
Sam Rasugu <mokuasamr@gmail.com>
2024-04-11 08:48:22 +0300

    feat: added business model

---
+++ models/business.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const businessSchema = new mongoose.Schema(
+  {
+    businessName: {
+      type: String,
+      required: true,
+    },
+    businessEmail: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    businessPhone: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Business = mongoose.model("Business", businessSchema);
+module.exports = {Business, businessSchema};
